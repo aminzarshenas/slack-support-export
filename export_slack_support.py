aa1e4a68324d9d8fd7876f651b81200182ab5606
aminzarshenas <amin.zarshenas@gmail.com>
2019-10-31 12:15:40 -0500

    Add reading messages and convert to dataframe.

--- export_slack_support.py
+++ export_slack_support.py
@@ -1,10 +1,10 @@
+from datetime import datetime
+from datetime import timedelta
 import json
 import os
-from datetime import datetime, timedelta
 
 import pandas as pd
 import slacker
-
 
 
 class SupportTracker:
@@ -18,6 +18,7 @@ class SupportTracker:
 
     TOKEN_ENV_VAR = "SLACK_API_KEY"
     MAX_PAGE_SIZE = 100
+    MAX_PAGE = 1
     REQUEST_MANAGER_NAME = "request manager"
 
     def __init__(self):
@@ -26,7 +27,7 @@ class SupportTracker:
         self.client.auth.test()
 
 
-    def get_request_messages(self, channel_name: str, min_date: datetime, max_date: datetime):
+    def get_messages(self, channel_name: str, min_date: datetime, max_date: datetime):
         """
         :param channel_name: name of the channel to look for the messages in
         :param min_date: minimum date to look for the messages
@@ -34,6 +35,11 @@ class SupportTracker:
 
         :return: return a pandas dataframe containing the messages and their fields
         """
+        messages = self._get_messages(channel_name, min_date, max_date)
+        messages = [self._parse_message(m) for m in messages]
+        messages = pd.DataFrame(messages)
+        messages = messages[["date_time", "iid", "username", "permalink", "text"]]
+        return messages
 
 
     def _get_messages(self, channel_name: str, min_date: datetime, max_date: datetime):
@@ -44,10 +50,69 @@ class SupportTracker:
 
         :return: return a list of dicts of messages and their fields
         """
+        page = self.MAX_PAGE
+        messages = []
+        stap = False
+        while page >= 0 and not stap:
+            res = self._get_one_page_messages(channel_name, page, self.MAX_PAGE_SIZE)
+            if res:
+                for msg in res:
+                    t = self._timestamp_string_to_datetime(msg["ts"])
+                    if t < min_date:
+                        stop = True
+                        break
+                    if t >= min_date and t < max_date:
+                        messages.append(msg)
+            page = page - 1
 
-        channel_id = self._get_channel_id(channel_name)
-        messages = self.client.channels.history(channel_id, latest=datetime.timestamp(max_date), oldest=datetime.timestamp(min_date)).body['messages']
+        messages.reverse()
         return messages
+
+
+    def _get_one_page_messages(self, channel_name: str, page: int, page_size: int):
+
+        """
+        :param channel_name: name of the channel to look for the messages in
+        :param page: starting page for pagination
+        :param page_size: page_size for pagination
+
+        :return: return a list of dicts of messages and their fields
+        """
+        res = self.client.search.messages(
+                    query=f'in:{channel_name}',
+                    sort='timestamp',
+                    sort_dir='desc',
+                    page=page,
+                    count=page_size
+                ).body['messages']['matches']
+        return res
+
+
+    def _parse_message(self, message: dict):
+
+        """
+        Extracts and converts the important fields of the message
+
+        :param message: message to be parsed
+
+        :return: modified message
+        """
+        keys = ["ts", "iid", "username", "permalink", "text"]
+        message = {k: message[k] for k in keys}
+        message["date_time"] = self._timestamp_string_to_datetime(message["ts"])
+        message["ts"] = None
+        return message
+
+
+    def _timestamp_string_to_datetime(self, timestamp_string: str):
+        """
+        :param timestamp_string: timestamp
+
+        :return: return the corresponding datetime
+        """
+        t = datetime.fromtimestamp(int(float(timestamp_string)))
+        return t
+
 
     def _get_channel_id(self, channel_name: str):
         """
@@ -100,34 +165,16 @@ class SupportTracker:
 
 channel_name = "platform-ds-slack-workflow-test"
 user_name = 'Request Manager'
+max_date = datetime(2019, 10, 31)
+min_date = max_date - timedelta(1)
 
 SLACK_API_KEY_FILE = os.environ["HOME"] + "/creds/slack-API-Key.txt"
 with open(SLACK_API_KEY_FILE, "r") as f:
     os.environ["SLACK_API_KEY"] = f.read()[:-1]
 
 st = SupportTracker()
-print(st._get_user_id_workflow(user_name))
-
-
-
-channel_name = "platform-ds-slack-workflow-test"
-user_name = 'request manager'
-
-
-# m = st.client.search.messages(
-#             query=f'in:{channel_name}',
-#             sort='timestamp',
-#             sort_dir='asc',
-#             page=0,
-#             count=1000
-#         )
-# for i in m.body['messages']['matches']:
-#     print((i['permalink'], datetime.fromtimestamp(int(float(i['ts'])))))
-
-# print(set([i['username'] for i in m.body['messages']['matches']]))
-
-# max_date = datetime(2019, 10, 31)
-# min_date = max_date - timedelta(1)
+# print(st._get_user_id_workflow(user_name))
+print(st.get_messages(channel_name, min_date, max_date).iloc[0, :])
 
 
 
